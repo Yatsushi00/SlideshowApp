d01b805338a6acf58f8fc53da94739cd40ec942d
Jun <atyd2018tojunkregister5353@gmail.com>
2021-06-16 16:38:51 +0900

    Initial Commit

---
+++ テスト/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  テスト
+//
+//  Created by USER on 2021/06/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
